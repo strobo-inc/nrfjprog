6bd56b947020002604508ff7d2d269bf6c3cca42
Michael <mjdietzx@gmail.com>
2016-03-20 13:17:44 +0100

    Refactored. Added comments, restructured a bit and added helper functions for adding arguments.

--- nordicsemi/__main__.py
+++ nordicsemi/__main__.py
@@ -31,16 +31,19 @@ import sys
 
 from lib import nrf5x
 
+NRFJPROG_DESCRIPTION = "nrfjprog is a command line tool used for programming nRF5x devices. It is implemented in Python and utilizes pynrfjprog, a Python wrapper for the nrfjprog DLL. Both nrfjprog and pynrfjprog are open source and can be found on Nordic's GitHub. To report an issue, request a feature, or contribute please see: https://github.com/mjdietzx/nrfjprog."
+NRFJPROG_EPILOG = "Just like any standard command line tool, one positional command can be specified, followed by it's specific arguments. To see arguments for a specific command type: python nordicsemi COMMAND -h (i.e. python nordicsemi erase -h)."
+
 def _add_erase_command(subparsers):
     """
     Adds the erase sub-command and it's command-line arguments to our top-level parser.
 
     """
     erase_parser = subparsers.add_parser('erase', help = 'Erases the device.')
-    _add_clockspeed_option(erase_parser)
+    _add_clockspeed_argument(erase_parser)
     _add_erase_group(erase_parser)
-    _add_quiet_option(erase_parser)
-    _add_snr_option(erase_parser)
+    _add_quiet_argument(erase_parser)
+    _add_snr_argument(erase_parser)
     erase_parser.set_defaults(func = nrf5x.erase)
 
 def _add_halt_command(subparsers):
@@ -49,8 +52,8 @@ def _add_halt_command(subparsers):
 
     """
     halt_parser = subparsers.add_parser('halt', help = 'Halts the CPU.')
-    _add_quiet_option(halt_parser)
-    _add_snr_option(halt_parser)
+    _add_quiet_argument(halt_parser)
+    _add_snr_argument(halt_parser)
     halt_parser.set_defaults(func = nrf5x.halt)
 
 def _add_ids_command(subparsers):
@@ -67,10 +70,10 @@ def _add_memrd_command(subparsers):
 
     """
     memrd_parser = subparsers.add_parser('memrd', help = 'Reads memory.')
-    _add_addr_option(memrd_parser)
-    _add_bytes_option(memrd_parser)
-    _add_quiet_option(memrd_parser)
-    _add_snr_option(memrd_parser)
+    _add_addr_argument(memrd_parser)
+    _add_length_argument(memrd_parser)
+    _add_quiet_argument(memrd_parser)
+    _add_snr_argument(memrd_parser)
     memrd_parser.set_defaults(func = nrf5x.memrd)
 
 def _add_memwr_command(subparsers):
@@ -79,11 +82,11 @@ def _add_memwr_command(subparsers):
 
     """
     memwr_parser = subparsers.add_parser('memwr', help = 'Writes to memory.')
-    _add_addr_option(memwr_parser)
-    _add_flash_option(memwr_parser)
-    _add_quiet_option(memwr_parser)
-    _add_snr_option(memwr_parser)
-    _add_val_option(memwr_parser)
+    _add_addr_argument(memwr_parser)
+    _add_flash_argument(memwr_parser)
+    _add_quiet_argument(memwr_parser)
+    _add_snr_argument(memwr_parser)
+    _add_val_argument(memwr_parser)
     memwr_parser.set_defaults(func = nrf5x.memwr)
 
 def _add_program_command(subparsers):
@@ -92,13 +95,13 @@ def _add_program_command(subparsers):
 
     """
     program_parser = subparsers.add_parser('program', help = 'Programs the device.')
-    _add_clockspeed_option(program_parser)
-    _add_file_option(program_parser)
+    _add_clockspeed_argument(program_parser)
+    _add_file_argument(program_parser)
     _add_erase_group(program_parser)
-    _add_quiet_option(program_parser)
-    _add_verify_option(program_parser)
+    _add_quiet_argument(program_parser)
+    _add_verify_argument(program_parser)
     _add_reset_group(program_parser)
-    _add_snr_option(program_parser)
+    _add_snr_argument(program_parser)
     program_parser.set_defaults(func = nrf5x.program)
 
 def _add_readregs_command(subparsers):
@@ -107,8 +110,8 @@ def _add_readregs_command(subparsers):
 
     """
     readregs_parser = subparsers.add_parser('readregs', help = 'Reads the CPU register.')
-    _add_quiet_option(readregs_parser)
-    _add_snr_option(readregs_parser)
+    _add_quiet_argument(readregs_parser)
+    _add_snr_argument(readregs_parser)
     readregs_parser.set_defaults(func = nrf5x.readregs)
 
 def _add_readtofile_command(subparsers):
@@ -117,12 +120,12 @@ def _add_readtofile_command(subparsers):
 
     """
     readtofile_parser = subparsers.add_parser('readtofile', help = 'Reads the CPU register.')
-    _add_file_option(readtofile_parser)
-    _add_quiet_option(readtofile_parser)
-    readtofile_parser.add_argument('--readcode', action = 'store_true', help = 'If this option is specified we will read code FLASH and store in FILE.')
-    readtofile_parser.add_argument('--readram', action = 'store_true', help = 'If this option is specified we will read RAM FLASH and store in FILE.')
-    readtofile_parser.add_argument('--readuicr', action = 'store_true', help = 'If this option is specified we will read UICR FLASH and store in FILE.')
-    _add_snr_option(readtofile_parser)
+    _add_file_argument(readtofile_parser)
+    _add_quiet_argument(readtofile_parser)
+    _add_readcode_argument(readtofile_parser)
+    _add_readram_argument(readtofile_parser)
+    _add_readuicr_argument(readtofile_parser)
+    _add_snr_argument(readtofile_parser)
     readtofile_parser.set_defaults(func = nrf5x.readtofile)
 
 def _add_recover_command(subparsers):
@@ -131,9 +134,9 @@ def _add_recover_command(subparsers):
 
     """
     recover_parser = subparsers.add_parser('recover', help = 'Erases all user FLASH and RAM and disables any readback protection mechanisms that are enabled.')
-    _add_clockspeed_option(recover_parser)
-    _add_quiet_option(recover_parser)
-    _add_snr_option(recover_parser)
+    _add_clockspeed_argument(recover_parser)
+    _add_quiet_argument(recover_parser)
+    _add_snr_argument(recover_parser)
     recover_parser.set_defaults(func = nrf5x.recover)
 
 def _add_reset_command(subparsers):
@@ -142,9 +145,9 @@ def _add_reset_command(subparsers):
 
     """
     reset_parser = subparsers.add_parser('reset', help = 'Resets the device.')
-    _add_quiet_option(reset_parser)
+    _add_quiet_argument(reset_parser)
     _add_reset_group(reset_parser)
-    _add_snr_option(reset_parser)
+    _add_snr_argument(reset_parser)
     reset_parser.set_defaults(func = nrf5x.reset)
 
 def _add_run_command(subparsers):
@@ -153,8 +156,8 @@ def _add_run_command(subparsers):
 
     """
     run_parser = subparsers.add_parser('run', help = 'Runs the CPU.')
-    _add_quiet_option(run_parser)
-    _add_snr_option(run_parser)
+    _add_quiet_argument(run_parser)
+    _add_snr_argument(run_parser)
     run_parser.set_defaults(func = nrf5x.run)
 
 def _add_verify_command(subparsers):
@@ -163,10 +166,10 @@ def _add_verify_command(subparsers):
 
     """
     verify_parser = subparsers.add_parser('verify', help = 'Verifies that memory contains the correct data.')
-    _add_clockspeed_option(verify_parser)
-    _add_quiet_option(verify_parser)
-    _add_snr_option(verify_parser)
-    _add_file_option(verify_parser)
+    _add_clockspeed_argument(verify_parser)
+    _add_quiet_argument(verify_parser)
+    _add_snr_argument(verify_parser)
+    _add_file_argument(verify_parser)
     verify_parser.set_defaults(func = nrf5x.verify)
 
 def _add_version_command(subparsers):
@@ -177,92 +180,96 @@ def _add_version_command(subparsers):
     version_parser = subparsers.add_parser('version', help = 'Display the nrfjprog and JLinkARM DLL versions.')
     version_parser.set_defaults(func = nrf5x.version)
 
-def _add_erase_group(sub_parsers):
+def _add_erase_group(subparsers):
     """
-    Adds the mutually exclusive group of erase options to our command.
+    Adds the mutually exclusive group of erase arguments to our command.
 
     """
-    erase_group = sub_parsers.add_mutually_exclusive_group()
-    erase_group.add_argument('-e', '--eraseall', action = 'store_true', help = 'Erase all user flash, including UICR, before programming the device.')
-    erase_group.add_argument('-s', '--sectorserase', action = 'store_true', help = 'Erase all sectors that FILE writes before programming.')
-    erase_group.add_argument('-u', '--sectorsanduicrerase', action = 'store_true', help = 'Erase all sectors that FILE writes and the UICR before programming.')
+    erase_group = subparsers.add_mutually_exclusive_group()
+    _add_eraseall_argument(erase_group)
+    _add_sectors_erase(erase_group)
+    _add_sectorsuicr_erase(erase_group)
 
-def _add_reset_group(sub_parsers):
+def _add_reset_group(subparsers):
     """
-    Adds the mutually exclusive group of reset options to our command.
-
-    """
-    reset_group = sub_parsers.add_mutually_exclusive_group()
-    reset_group.add_argument('-d', '--debugreset', action = 'store_true', help = 'Executes a debug reset.')
-    reset_group.add_argument('-p', '--pinreset', action = 'store_true', help = 'Executes a pin reset.')
-    reset_group.add_argument('-r', '--systemreset', action = 'store_true', help = 'Executes a system reset.')
-
-def _add_addr_option(sub_parsers):
-    """
-    Adds the required addr option to our command.
+    Adds the mutually exclusive group of reset arguments to our command.
 
     """
-    sub_parsers.add_argument('-a', '--addr', type = int, help = 'The address in memory to be read/write.', required = True)
+    reset_group = subparsers.add_mutually_exclusive_group()
+    _add_debugreset_argument(reset_group)
+    _add_pinreset_argument(reset_group)
+    _add_sysreset_argument(reset_group)
 
-def _add_bytes_option(sub_parsers):
+"""
+The add_argument helper functions. They define how a single command-line argument should be parsed. These are all optional arguments.
+
+"""
+
+def _add_addr_argument(subparsers):
+    subparsers.add_argument('-a', '--addr', type = int, help = 'The address in memory to be read/written.', required = True)
+
+def _add_clockspeed_argument(subparsers):
+    subparsers.add_argument('-c', '--clockspeed', type = int, help = 'Sets the debugger SWD clock speed in kHz for the operation.')
+
+def _add_debugreset_argument(subparsers):
+    subparsers.add_argument('-d', '--debugreset', action = 'store_true', help = 'Executes a debug reset.')
+
+def _add_eraseall_argument(subparsers):
+    subparsers.add_argument('-e', '--eraseall', action = 'store_true', help = 'Erase all user FLASH including UICR.')
+
+def _add_file_argument(subparsers):
+    subparsers.add_argument('-f', '--file', type = file, help = 'The hex file to be used in this operation.', required = True)
+
+def _add_flash_argument(subparsers):
+    subparsers.add_argument('--flash', action = 'store_true', help = 'If this argument is specified write to FLASH using the NVMC. Else write to RAM.')
+
+def _add_length_argument(subparsers):
+    subparsers.add_argument('-l', '--length', type = int, help = 'The number of bytes to be read. 4 (one word) by default.', default = 4)
+
+def _add_pinreset_argument(subparsers):
+    subparsers.add_argument('-p', '--pinreset', action = 'store_true', help = 'Executes a pin reset.')
+
+def _add_quiet_argument(subparsers):
+    subparsers.add_argument('-q', '--quiet', action =  'store_true', help = 'Nothing will be printed to terminal during the operation.' )
+
+def _add_readcode_argument(subparsers):
+    subparsers.add_argument('--readcode', action = 'store_true', help = 'If this argument is specified read code FLASH and store in FILE.')
+
+def _add_readram_argument(subparsers):
+    subparsers.add_argument('--readram', action = 'store_true', help = 'If this argument is specified read RAM and store in FILE.')
+
+def _add_readuicr_argument(subparsers):
+    subparsers.add_argument('--readuicr', action = 'store_true', help = 'If this argument is specified read UICR FLASH and store in FILE.')
+
+def _add_sectors_erase(subparsers):
+    subparsers.add_argument('-se', '--sectorserase', action = 'store_true', help = 'Erase all sectors that FILE contains data in before programming.')
+
+def _add_sectorsuicr_erase(subparsers):
+    subparsers.add_argument('-u', '--sectorsanduicrerase', action = 'store_true', help = 'Erase all sectors that FILE contains data in and the UICR (unconditionally) before programming.')
+
+def _add_snr_argument(subparsers):
+    subparsers.add_argument('-s', '--snr', type = int, help = 'Selects the debugger with the given serial number among all those connected to the PC for the operation.')
+
+def _add_sysreset_argument(subparsers):
+    subparsers.add_argument('-r', '--systemreset', action = 'store_true', help = 'Executes a system reset.')
+
+def _add_val_argument(subparsers):
+    subparsers.add_argument('--val', type = int, help = 'The 32 bit word to be written to memory.', required = True)
+
+def _add_verify_argument(subparsers):
+    subparsers.add_argument('-v', '--verify', action = 'store_true', help = 'Read back memory and verify that it matches FILE.')
+
+"""
+The top-level functionality of our command-line application.
+
+"""
+
+def _add_commands(subparsers):
     """
-    Adds the bytes option to our command.
-
+    Split up the functionality of nrfjprog into multiple sub-commands because nrfjprog performs several different functions which require different command-line arguments.
+    
+    :param Special Action Object subparsers: https://docs.python.org/3/library/argparse.html#sub-commands
     """
-    sub_parsers.add_argument('-b', '--bytes', type = int, help = 'The number of bytes to be read. By default 4, one word.', default = 4)
-
-def _add_clockspeed_option(sub_parsers):
-    """
-    Adds the clockspeed option to our command.
-
-    """
-    sub_parsers.add_argument('-c', '--clockspeed', type = int, help = 'Sets the debugger SWD clock speed in kHz.')
-
-def _add_file_option(sub_parsers):
-    """
-    Adds the required file option to our command.
-
-    """
-    sub_parsers.add_argument('-f', '--file', type = file, help = 'The hex file to be programmed to the device.', required = True)
-
-def _add_flash_option(sub_parsers):
-    """
-    Adds the flash option to our command.
-
-    """
-    sub_parsers.add_argument('--flash', action = 'store_true', help = 'If this option is specified we will write to FLASH. If not we will write to RAM.')
-
-def _add_quiet_option(sub_parsers):
-    """
-    Adds the quiet option to our command.
-
-    """
-    sub_parsers.add_argument('-q', '--quiet', action =  'store_true', help = 'Will not print to terminal.' )
-
-def _add_snr_option(sub_parsers):
-    """
-    Adds the snr option to our command.
-
-    """
-    sub_parsers.add_argument('--snr', type = int, help = 'Selects the debugger with the given serial number among all those connected to the PC for the operation.')
-
-def _add_val_option(sub_parsers):
-    """
-    Adds the value option to our command.
-
-    """
-    sub_parsers.add_argument('--val', type = int, help = 'The data word to be written to memory.', required = True)
-
-def _add_verify_option(sub_parsers):
-    """
-    Adds the verify option to our command.
-
-    """
-    sub_parsers.add_argument('-v', '--verify', action = 'store_true', help = 'Read back memory after programming and verify that FILE was correctly written.')
-
-def main(argv):
-    parser = argparse.ArgumentParser(description='nrfjprog is a command line tool used for programming nRF5x devices.')
-    subparsers = parser.add_subparsers()
     _add_erase_command(subparsers)
     _add_halt_command(subparsers)
     _add_ids_command(subparsers)
@@ -277,8 +284,20 @@ def main(argv):
     _add_verify_command(subparsers)
     _add_version_command(subparsers)
 
+def main(argv):
+    """
+    Set up a command line interface using the argparse module.
+
+    Above we will define what arguments our program requires and argparse will figure out how to parse those from sys.argv.
+    For info on argparse see: https://docs.python.org/3/library/argparse.html.
+    """
+    parser = argparse.ArgumentParser(description = NRFJPROG_DESCRIPTION, epilog = NRFJPROG_EPILOG)
+    subparsers = parser.add_subparsers()
+
+    _add_commands(subparsers)
+
     args = parser.parse_args()
-    args.func(args)  # call the default function
+    args.func(args)  # Call the default function. (i.e. nRF5x.erase() in the erase command).
 
 if __name__ == '__main__':
     main(sys.argv)
