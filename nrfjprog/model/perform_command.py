6618964482bc9c2847cc802fb03a5ca799126f50
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-03-29 16:02:58 +0200

    undo.

--- nrfjprog/model/perform_command.py
+++ nrfjprog/model/perform_command.py
@@ -27,7 +27,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
-from nrfjprog import nrfjprog_version
+import nrfjprog_version
 import numpy as np
 from pynrfjprog import API, Hex
 
