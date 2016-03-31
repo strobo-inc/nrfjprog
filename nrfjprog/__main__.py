55fe68ecf579a22e835eddee8ae055069513a73d
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-03-31 15:08:08 +0200

    nevermind, undo last commit these should be internal.

--- nrfjprog/__main__.py
+++ nrfjprog/__main__.py
@@ -40,9 +40,6 @@ class Nrfjprog(object):
     """
     Class that handles the command-line interface.
 
-    This class may also be used to build an internal version of nrfjprog.exe (either by Nordic or a user).
-    When using this class all the positional commands will be present, hence the protected naming of _add_commands.
-    Methods to add groups of arguments and arguments are public so the user can reuse these if they require the same argument with one of their custom commands.
     """
 
     nrfjprog_description = "nrfjprog is a command line tool used for programming nRF5x devices. It is implemented in Python and utilizes pynrfjprog, a Python wrapper for the nrfjprog DLL. Both nrfjprog and pynrfjprog are open source and can be found on Nordic's GitHub. To report an issue, request a feature, or contribute please see: https://github.com/mjdietzx/nrfjprog."
@@ -68,11 +65,11 @@ class Nrfjprog(object):
         @param func           callback: Function that performs operation for given command.
         @param boolean        connects: If this command connects to the emulator (debugger) and should have the option to set the clock speed/serial number.
         """
-        self.add_quiet_argument(parser)
+        self._add_quiet_argument(parser)
 
         if connects:
-            self.add_clockspeed_argument(parser)
-            self.add_snr_argument(parser)
+            self._add_clockspeed_argument(parser)
+            self._add_snr_argument(parser)
 
         parser.set_defaults(func=callback)
 
@@ -108,7 +105,7 @@ class Nrfjprog(object):
 
 
     """
-    The top-level positional commands of our command-line interface.
+    The top-level positional commands of our command-line interface. These add shared arguments with Command() and then add unique arguments.
 
     """
 
@@ -116,7 +113,7 @@ class Nrfjprog(object):
         erase_parser = self.subparsers.add_parser('erase', help="Erases the device's FLASH.")
         self.add_common_properties_to_command(erase_parser, perform_command.erase)
 
-        self.add_erase_group(erase_parser)
+        self._add_erase_group(erase_parser)
 
     def _add_halt_command(self):
         halt_parser = self.subparsers.add_parser('halt', help="Halts the device's CPU.")
@@ -130,15 +127,15 @@ class Nrfjprog(object):
         memrd_parser = self.subparsers.add_parser('memrd', help="Reads the device's memory.")
         self.add_common_properties_to_command(memrd_parser, perform_command.memrd)
 
-        self.add_addr_argument(memrd_parser)
-        self.add_length_argument(memrd_parser)
+        self._add_addr_argument(memrd_parser)
+        self._add_length_argument(memrd_parser)
 
     def _add_memwr_command(self):
         memwr_parser = self.subparsers.add_parser('memwr', help="Writes one word in the device's memory.")
         self.add_common_properties_to_command(memwr_parser, perform_command.memwr)
 
-        self.add_addr_argument(memwr_parser)
-        self.add_val_argument(memwr_parser)
+        self._add_addr_argument(memwr_parser)
+        self._add_val_argument(memwr_parser)
 
     def _add_pinresetenable_command(self):
         pinresetenable_parser = self.subparsers.add_parser('pinresetenable', help="Enable the pin reset (GPIO 21) on nRF52 devices. Invalid command on nRF51 devices.")
@@ -148,16 +145,16 @@ class Nrfjprog(object):
         program_parser = self.subparsers.add_parser('program', help='Programs the device.')
         self.add_common_properties_to_command(program_parser, perform_command.program)
 
-        self.add_file_argument(program_parser)
-        self.add_erase_before_flash_group(program_parser)
-        self.add_verify_argument(program_parser)
-        self.add_reset_group(program_parser)
+        self._add_file_argument(program_parser)
+        self._add_erase_before_flash_group(program_parser)
+        self._add_verify_argument(program_parser)
+        self._add_reset_group(program_parser)
 
     def _add_readback_command(self):
         readback_parser = self.subparsers.add_parser('rbp', help='Enables the readback protection mechanism.')
         self.add_common_properties_to_command(readback_parser, perform_command.readback)
 
-        self.add_rbplevel_argument(readback_parser)
+        self._add_rbplevel_argument(readback_parser)
 
     def _add_readregs_command(self):
         readregs_parser = self.subparsers.add_parser('readregs', help='Reads the CPU registers.')
@@ -167,35 +164,35 @@ class Nrfjprog(object):
         readtofile_parser = self.subparsers.add_parser('readtofile', help="Reads and stores the device's memory.")
         self.add_common_properties_to_command(readtofile_parser, perform_command.readtofile)
 
-        self.add_file_argument(readtofile_parser)
-        self.add_readcode_argument(readtofile_parser)
-        self.add_readram_argument(readtofile_parser)
-        self.add_readuicr_argument(readtofile_parser)
+        self._add_file_argument(readtofile_parser)
+        self._add_readcode_argument(readtofile_parser)
+        self._add_readram_argument(readtofile_parser)
+        self._add_readuicr_argument(readtofile_parser)
 
     def _add_recover_command(self):
         recover_parser = self.subparsers.add_parser('recover', help='Erases all user FLASH and RAM and disables any readback protection mechanisms that are enabled.')
         self.add_common_properties_to_command(recover_parser, perform_command.recover)
 
-        self.add_family_argument(recover_parser)
+        self._add_family_argument(recover_parser)
 
     def _add_reset_command(self):
         reset_parser = self.subparsers.add_parser('reset', help='Resets the device.')
         self.add_common_properties_to_command(reset_parser, perform_command.reset)
 
-        self.add_reset_group(reset_parser)
+        self._add_reset_group(reset_parser)
 
     def _add_run_command(self):
         run_parser = self.subparsers.add_parser('run', help="Runs the device's CPU.")
         self.add_common_properties_to_command(run_parser, perform_command.run)
 
-        self.add_pc_argument(run_parser)
-        self.add_sp_argument(run_parser)
+        self._add_pc_argument(run_parser)
+        self._add_sp_argument(run_parser)
 
     def _add_verify_command(self):
         verify_parser = self.subparsers.add_parser('verify', help="Verifies that the device's memory contains the correct data.")
         self.add_common_properties_to_command(verify_parser, perform_command.verify)
 
-        self.add_file_argument(verify_parser)
+        self._add_file_argument(verify_parser)
 
     def _add_version_command(self):
         version_parser = self.subparsers.add_parser('version', help='Display the nrfjprog and JLinkARM DLL versions.')
@@ -207,23 +204,23 @@ class Nrfjprog(object):
 
     """
 
-    def add_erase_group(self, parser):
+    def _add_erase_group(self, parser):
         erase_group = parser.add_mutually_exclusive_group()
-        self.add_eraseall_argument(erase_group)
-        self.add_erasepage_argument(erase_group)
-        self.add_eraseuicr_argument(erase_group)
+        self._add_eraseall_argument(erase_group)
+        self._add_erasepage_argument(erase_group)
+        self._add_eraseuicr_argument(erase_group)
 
-    def add_erase_before_flash_group(self, parser):
+    def _add_erase_before_flash_group(self, parser):
         erase_before_flash_group = parser.add_mutually_exclusive_group()
-        self.add_eraseall_argument(erase_before_flash_group)
-        self.add_sectors_erase_argument(erase_before_flash_group)
-        self.add_sectorsuicr_erase_argument(erase_before_flash_group)
+        self._add_eraseall_argument(erase_before_flash_group)
+        self._add_sectors_erase_argument(erase_before_flash_group)
+        self._add_sectorsuicr_erase_argument(erase_before_flash_group)
 
-    def add_reset_group(self, parser):
+    def _add_reset_group(self, parser):
         reset_group = parser.add_mutually_exclusive_group()
-        self.add_debugreset_argument(reset_group)
-        self.add_pinreset_argument(reset_group)
-        self.add_sysreset_argument(reset_group)
+        self._add_debugreset_argument(reset_group)
+        self._add_pinreset_argument(reset_group)
+        self._add_sysreset_argument(reset_group)
 
 
     """
@@ -232,95 +229,95 @@ class Nrfjprog(object):
     """
 
     @classmethod
-    def add_addr_argument(cls, parser):
+    def _add_addr_argument(cls, parser):
         parser.add_argument('-a', '--addr', type=cls.auto_int, help='The address in memory to be read/written.', required=True)
 
     @classmethod
-    def add_clockspeed_argument(cls, parser):
+    def _add_clockspeed_argument(cls, parser):
         parser.add_argument('-c', '--clockspeed', type=int, metavar='CLOCKSPEEDKHZ', help='Sets the debugger SWD clock speed in kHz for the operation.')
 
     @classmethod
-    def add_debugreset_argument(cls, parser):
+    def _add_debugreset_argument(cls, parser):
         parser.add_argument('-d', '--debugreset', action='store_true', help='Executes a debug reset.')
 
     @classmethod
-    def add_eraseall_argument(cls, parser):
+    def _add_eraseall_argument(cls, parser):
         parser.add_argument('-e', '--eraseall', action='store_true', help='Erase all user FLASH including UICR.')
 
     @classmethod
-    def add_erasepage_argument(cls, parser):
+    def _add_erasepage_argument(cls, parser):
         parser.add_argument('--erasepage', type=cls.auto_int, metavar='PAGESTARTADDR', help='Erase the page starting at the address PAGESTARTADDR.')
 
     @classmethod
-    def add_eraseuicr_argument(cls, parser):
+    def _add_eraseuicr_argument(cls, parser):
         parser.add_argument('--eraseuicr', action='store_true', help='Erase the UICR page in FLASH.')
 
     @classmethod
-    def add_family_argument(cls, parser):
+    def _add_family_argument(cls, parser):
         parser.add_argument('--family', type=str, help='The family of the target device.', required=True, choices=['NRF51', 'NRF52'])
 
     @classmethod
-    def add_file_argument(cls, parser):
+    def _add_file_argument(cls, parser):
         parser.add_argument('-f', '--file', help='The hex file to be used in this operation.', required=True)
 
     @classmethod
-    def add_length_argument(cls, parser):
+    def _add_length_argument(cls, parser):
         parser.add_argument('-l', '--length', type=cls.auto_int, help='The number of bytes to be read. 4 (one word) by default.', default=4)
 
     @classmethod
-    def add_pc_argument(cls, parser):
+    def _add_pc_argument(cls, parser):
         parser.add_argument('--pc', type=cls.auto_int, metavar='PC_ADDR', help='Initial program counter to start the CPU running from.')
 
     @classmethod
-    def add_pinreset_argument(cls, parser):
+    def _add_pinreset_argument(cls, parser):
         parser.add_argument('-p', '--pinreset', action='store_true', help='Executes a pin reset.')
 
     @classmethod
-    def add_quiet_argument(cls, parser):
+    def _add_quiet_argument(cls, parser):
         parser.add_argument('-q', '--quiet', action='store_true', help='Nothing will be printed to terminal during the operation.')
 
     @classmethod
-    def add_rbplevel_argument(cls, parser):
+    def _add_rbplevel_argument(cls, parser):
         parser.add_argument('--rbplevel', help='Specify the read back protection level (NRF51 only).', choices=['CR0', 'ALL'])
 
     @classmethod
-    def add_readcode_argument(cls, parser):
+    def _add_readcode_argument(cls, parser):
         parser.add_argument('--readcode', action='store_true', help='If this argument is specified read code FLASH and store in FILE.')
 
     @classmethod
-    def add_readram_argument(cls, parser):
+    def _add_readram_argument(cls, parser):
         parser.add_argument('--readram', action='store_true', help='If this argument is specified read RAM and store in FILE.')
 
     @classmethod
-    def add_readuicr_argument(cls, parser):
+    def _add_readuicr_argument(cls, parser):
         parser.add_argument('--readuicr', action='store_true', help='If this argument is specified read UICR FLASH and store in FILE.')
 
     @classmethod
-    def add_sectors_erase_argument(cls, parser):
+    def _add_sectors_erase_argument(cls, parser):
         parser.add_argument('-se', '--sectorserase', action='store_true', help='Erase all sectors that FILE contains data in before programming.')
 
     @classmethod
-    def add_sectorsuicr_erase_argument(cls, parser):
+    def _add_sectorsuicr_erase_argument(cls, parser):
         parser.add_argument('-u', '--sectorsanduicrerase', action='store_true', help='Erase all sectors that FILE contains data in and the UICR (unconditionally) before programming.')
 
     @classmethod
-    def add_snr_argument(cls, parser):
+    def _add_snr_argument(cls, parser):
         parser.add_argument('-s', '--snr', type=int, help='Selects the debugger with the given serial number among all those connected to the PC for the operation.')
 
     @classmethod
-    def add_sp_argument(cls, parser):
+    def _add_sp_argument(cls, parser):
         parser.add_argument('--sp', type=cls.auto_int, metavar='SP_ADDR', help='Initial stack pointer.')
 
     @classmethod
-    def add_sysreset_argument(cls, parser):
+    def _add_sysreset_argument(cls, parser):
         parser.add_argument('-r', '--systemreset', action='store_true', help='Executes a system reset.')
 
     @classmethod
-    def add_val_argument(cls, parser):
+    def _add_val_argument(cls, parser):
         parser.add_argument('--val', type=cls.auto_int, help='The 32 bit word to be written to memory.', required=True)
 
     @classmethod
-    def add_verify_argument(cls, parser):
+    def _add_verify_argument(cls, parser):
         parser.add_argument('-v', '--verify', action='store_true', help='Read back memory and verify that it matches FILE.')
 
 
