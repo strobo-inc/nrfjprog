73f05aa7405666001f6a6737fa5ff763dc5fb4dd
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-04-01 14:02:45 +0200

    Improve the test fixture.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -26,28 +26,81 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+"""
+Test nrfjprog.exe that was built from our repo.
+
+Must add tests/dist on system running this script where dist/ contains the built exe and all it's dependencies.
+"""
+
 import subprocess
+import sys
 import unittest
 
+from pynrfjprog import API
 
-PATH_TO_EXE = "dist_windows_64\\nrfjprog.exe"
+
+if sys.platform.lower().startswith('win'):
+    PATH_TO_EXE = "dist\\windows_64\\nrfjprog.exe"
+elif sys.platform.lower().startswith('linux'):
+    PATH_TO_EXE = "dist\\linux_64\\nrfjprog"
+elif sys.platform.lower().startswith('dar'):
+    PATH_TO_EXE = "dist\\mac_osx_64\\nrfjprog"
 
 
 def run_exe(cmd):
-    subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    """
+    Run nrfjprog with the given commands.
 
+    :param List cmd: Commands to run nrfjprog with.
+    """
+    command = []
+    command.append(PATH_TO_EXE)
+    command.extend(cmd)
+    return subprocess.call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def setup_api():
+    """
+    Initialize api and connect to the target device.
+
+    :return Object api: Instance of API that is initialized and connected to the target device.
+    """
+    api = API.API('NRF52')
+    api.open()
+    api.connect_to_emu_without_snr()
+    return api
 
 class TestHelpScreens(unittest.TestCase):
+    """
 
+
+    """
     def test_help(self):
-        cmd = ([PATH_TO_EXE, "-h"])
-        run_exe(cmd)
+        self.assertTrue(run_exe(["-h"]) == 0)
 
     def test_command_help(self):
-        cmd = ([PATH_TO_EXE, "program", "-h"])
-        run_exe(cmd)
+        self.assertTrue(run_exe(["program", "-h"]) == 0)
 
+class TestEraseCommand(unittest.TestCase):
+    """
+
+
+    """
+    def test_erase_help(self):
+        self.assertTrue(run_exe(["erase", "-h"]) == 0)
+
+    def test_erase(self):
+        api = setup_api()
+        
+        api.write_u32(0x0, 0x0, True)
+
+        run_exe(["erase"])
+
+        self.assertTrue(api.read_u32(0x0) == 0xFFFFFFFF)
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestHelpScreens)
+    """
+
+
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestEraseCommand)
     unittest.TextTestRunner(verbosity=2).run(suite)
