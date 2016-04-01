71e389a717e5099cad4000e0b6fe5a8f9e864e99
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-04-01 09:33:47 +0200

    Device specs are correct for each version now.

--- nrfjprog/model/device.py
+++ nrfjprog/model/device.py
@@ -36,20 +36,20 @@ FLASH_SIZE = {'NRF52_FP1'      : 0x80000, # TODO: Fix these numbers.
               'NRF52_FP1_ENGA' : 0x80000,
               'NRF51_XLR3LC'   : 0x40000,
               'NRF51_XLR3P'    : 0x40000,
-              'NRF51_L3'       : 0x40000,
+              'NRF51_L3'       : 0x20000,
               'NRF51_XLR3'     : 0x40000,
               'NRF51_XLR2'     : 0x40000,
               'NRF51_XLR1'     : 0x40000}
 
 RAM_SIZE = {  'NRF52_FP1'      : 0x10000,
               'NRF52_FP1_ENGB' : 0x8000,
-              'NRF52_FP1_ENGA' : 0x8000,
-              'NRF51_XLR3LC'   : 0x8000,
+              'NRF52_FP1_ENGA' : 0x4000,
+              'NRF51_XLR3LC'   : 0x4000,
               'NRF51_XLR3P'    : 0x8000,
-              'NRF51_L3'       : 0x8000,
-              'NRF51_XLR3'     : 0x8000,
-              'NRF51_XLR2'     : 0x8000,
-              'NRF51_XLR1'     : 0x8000}
+              'NRF51_L3'       : 0x4000,
+              'NRF51_XLR3'     : 0x4000,
+              'NRF51_XLR2'     : 0x4000,
+              'NRF51_XLR1'     : 0x4000}
 
 PAGE_SIZE = { 'NRF52_FP1'      : 0x1000,
               'NRF52_FP1_ENGB' : 0x1000,
