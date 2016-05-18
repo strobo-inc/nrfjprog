7278c55275b677fd149ab0ad252a75c22e49a91e
Michael Dietz <Michael.Dietz@nordicsemi.no>
2016-05-18 12:17:04 +0200

    Create setup.py

--- setup.py
+++ setup.py
@@ -28,4 +28,48 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# TODO
+"""
+Setup script for pynrfjprog.
+
+USAGE:
+    python setup.py install
+"""
+
+import os
+from setuptools import setup, find_packages
+import sys
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def read_requirements(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).readlines()
+
+setup(
+    name ='nrfjprog',
+    version = nrfjprog.__version__,
+    description = 'The nrfjprog command line tool implemented in Python.',
+    long_description=read('README.md'),
+    url = 'http://www.nordicsemi.com/',
+    author = 'Nordic Semiconductor ASA',
+    license = 'BSD',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Embedded Systems',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords = 'nRF5 nRF51 nRF52 nrfjprog pynrfjprog pyOCD Nordic Semiconductor SEGGER JLink',
+    install_requires = read_requirements('requirements.txt'),
+    packages = find_packages()
+    # package_data = {}
+)
+
